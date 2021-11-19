f78da72693e0273da1be51e3ed3b74f59f385190
emorilebo <emorylebo@gmail.com>
2021-11-19 04:05:26 +0100

    Project linked with backend

--- pwj-module-8-my-blog-app-master/exercises/js/index.js
+++ pwj-module-8-my-blog-app-master/exercises/js/index.js
@@ -20,10 +20,12 @@ const getPosts = () => {
 const buildPosts = (blogPosts) => {
     let blogPostContent = "";
   for(blogPost of blogPosts){
-      const postDate = new Date(parseInt(blogPost.added_date)).toDateString()
+      const postDate = new Date(parseInt(blogPost.added_date)).toDateString();
+      const postImage = `${API_BASE_URL}${blogPost.post_image}`
+
     blogPostContent += `
     <div class="post">
-    <div class="post-image"></div>
+    <div class="post-image" style="background-image: url(${postImage})"></div>
     <div class="post-content">
         <div class="post-date">${postDate}</div>
         <div class="post-title"><h4>${blogPost.title}</h4></div>
